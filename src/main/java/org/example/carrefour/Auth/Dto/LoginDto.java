253b804dd68c65df7fc4f28ea9524d92b6443348
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-27 12:50:07 -0300

    create login dto to allow users to log in

---
+++ src/main/java/org/example/carrefour/Auth/Dto/LoginDto.java
@@ -0,0 +1,13 @@
+package org.example.carrefour.Auth.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String email;
+    private String password;
+}
